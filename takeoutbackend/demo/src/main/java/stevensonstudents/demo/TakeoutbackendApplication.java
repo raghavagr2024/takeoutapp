415a77791e5e7bbd756d134799471ff1780632ee
Raghav Agrawal <79773873+raghavagr2024@users.noreply.github.com>
2023-10-22 18:26:04 -0500

    added backend files

---
+++ takeoutbackend/demo/src/main/java/stevensonstudents/demo/TakeoutbackendApplication.java
@@ -0,0 +1,13 @@
+package stevensonstudents.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeoutbackendApplication.class, args);
+	}
+
+}
